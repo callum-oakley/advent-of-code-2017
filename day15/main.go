bae20719b0da0c1a23f2a951f628ef8c4026d126
Callum Oakley <c.oakley108@gmail.com>
2017-12-18 21:13:23 +0000

    sadly not faster, but neat

--- day15/main.go
+++ day15/main.go
@@ -17,31 +17,46 @@ var (
 )
 
 func main() {
-	var startA, startB, count1, count2, a, b int
-	fmt.Scanf("Generator A starts with %v", &startA)
-	fmt.Scanf("Generator B starts with %v", &startB)
-	a, b = startA, startB
-	for i := 0; i < sample1; i++ {
-		a, b = generate(a, factorA, alwaysTrue), generate(b, factorB, alwaysTrue)
-		if uint16(a) == uint16(b) {
-			count1++
+	var a, b int
+	fmt.Scanf("Generator A starts with %v", &a)
+	fmt.Scanf("Generator B starts with %v", &b)
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go func() {
+		chA := generate(a, factorA, alwaysTrue)
+		chB := generate(b, factorB, alwaysTrue)
+		count := 0
+		for i := 0; i < sample1; i++ {
+			if uint16(<-chA) == uint16(<-chB) {
+				count++
+			}
 		}
-	}
-	a, b = startA, startB
-	for i := 0; i < sample2; i++ {
-		a, b = generate(a, factorA, conditionA), generate(b, factorB, conditionB)
-		if uint16(a) == uint16(b) {
-			count2++
+		ch1 <- count
+	}()
+	go func() {
+		chA := generate(a, factorA, conditionA)
+		chB := generate(b, factorB, conditionB)
+		count := 0
+		for i := 0; i < sample2; i++ {
+			if uint16(<-chA) == uint16(<-chB) {
+				count++
+			}
 		}
-	}
-	fmt.Printf("part 1: %v\n", count1)
-	fmt.Printf("part 1: %v\n", count2)
+		ch2 <- count
+	}()
+	fmt.Printf("part 1: %v\n", <-ch1)
+	fmt.Printf("part 2: %v\n", <-ch2)
 }
 
-func generate(n, factor int, condition func(int) bool) int {
-	n = n * factor % divisor
-	for !condition(n) {
-		n = n * factor % divisor
-	}
-	return n
+func generate(n, factor int, condition func(int) bool) chan int {
+	ch := make(chan int, 128)
+	go func() {
+		for {
+			n = n * factor % divisor
+			if condition(n) {
+				ch <- n
+			}
+		}
+	}()
+	return ch
 }
